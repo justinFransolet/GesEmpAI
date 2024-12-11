16281f81c30df3dd81d8659aa834a6b20a5653fb
Q220036 <j.fransolet@student.helmo.be>
2024-12-11 10:46:53 +0100

    Ajout de l'imputation

---
+++ Normalization/Imputation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def impute_median(df: pd.DataFrame, column: str) -> None:
+    """
+    Impute the median value for missing values in the specified columns
+
+    :param df: DataFrame to impute missing values
+    :param column: Column to impute missing values
+    """
+
+    median_num_comp = df[column].median()
+    df['NumCompaniesWorked'].fillna(median_num_comp, inplace=True)
