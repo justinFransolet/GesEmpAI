2ec4ef35062ef04653fa8bc969cfb55803242573
Q220036 <j.fransolet@student.helmo.be>
2024-12-11 10:55:19 +0100

    Outil de fusion de df

---
+++ Normalization/Smithy.py
@@ -0,0 +1,6 @@
+﻿from typing import Literal
+
+import pandas as pd
+
+def merge_dataframe(df1: pd.DataFrame, df2: pd.DataFrame, column: str,method: Literal["left", "right", "inner", "outer", "cross"] = "left") -> pd.DataFrame:
+    return pd.merge(df1, df2, on=column, how=method)
